16af859ab87634376cb52c9b8047f5e3ebef45e4
ienglehart <ienglehart13@gmail.com>
2021-01-07 15:28:58 -0500

    indexed.db added
    indexed.db added for offline functionality and started adding service worker

---
+++ Develop/public/js/service-worker.js
@@ -0,0 +1,17 @@
+const APP_PREFIX = 'BudgetTracker-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+const FILES_TO_CACHE = [
+    //cached files here
+
+];
+
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+})
